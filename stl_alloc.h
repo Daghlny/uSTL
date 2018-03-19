420be9d49bc0e0a6a8c00f798592aef5c236fa40
Daghlny <daghlny@gmail.com>
2018-03-19 20:53:35 +0800

    finish stl_alloc.h

--- stl_alloc.h
+++ stl_alloc.h
@@ -92,9 +92,9 @@ enum {__ALIGN = 8};
 enum {__MAX_BYTES = 128};
 enum {__NFREELISTS = __MAX_BYTES/__ALIGN};
 
-template<bool threads>
 class __default_alloc_template {
     private:
+        // adjust the nbytes into the closest __ALIGN's multiple
         static size_t round_up(size_t nbytes) {
             return (((nbytes) + __ALIGN-1) & ~(__ALIGN-1));
         }
@@ -116,9 +116,9 @@ class __default_alloc_template {
         static void* __refill(size_t n);
         static char* __chunk_alloc(size_t size, int &n);
 
-        static char *start_free;
-        static char *end_free;
-        static size_t heap_size;
+        static char *start_free;        // the begin of memory pool
+        static char *end_free;          // the end of memory pool
+        static size_t heap_size;        // the size of history memory space size added into memory pool
 
     public:
         static void *allocate(size_t n) {
@@ -131,8 +131,10 @@ class __default_alloc_template {
 
             my_free_list = free_list + freelist_index(n);
             result = *my_free_list;
+
+            // if there is no more space in the certain freelist
             if (result == 0) {
-                void *r = refill(round_up(n));
+                void *r = __refill(round_up(n));
                 return r;
             }
 
@@ -149,6 +151,7 @@ class __default_alloc_template {
                 return;
             }
 
+            // insert the block into the head of certain freelist
             my_free_list = free_list + freelist_index(n);
             q->free_list_link = *my_free_list;
             *my_free_list = q;
@@ -156,27 +159,28 @@ class __default_alloc_template {
 
         static void *reallocate(void *p, size_t old_sz, size_t new_sz);
 
-        void* refill(size_t n);
+        //void* __refill(size_t n);
+        //char* __chunk_alloc(size_t size, int& nobjs);
 };
 
-template<bool threads>
-char *__default_alloc_template<threads>::start_free = 0;
+char *__default_alloc_template::start_free = 0;
 
-template<bool threads>
-char *__default_alloc_template<threads>::end_free = 0;
+char *__default_alloc_template::end_free = 0;
 
-template<bool threads>
-size_t __default_alloc_template<threads>::heap_size = 0;
+size_t __default_alloc_template::heap_size = 0;
 
-template<bool threads>
-__default_alloc_template<threads>::__obj *volatile
-__default_alloc_template<threads>::free_lists[__NFREELISTS] = 
+__default_alloc_template::__obj *volatile
+__default_alloc_template::free_lists[__NFREELISTS] = 
 {0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0};
 
-template<bool threads>
-void* __default_alloc_template<threads>::refile(size_t n) {
+
+// \n must be __ALIGN's multiple
+// __refill the freelist with the size of \n
+void* __default_alloc_template::__refill(size_t n) {
+
     int nobjs = 20;
     char *chunk = __chunk_alloc(n, nobjs);
+
     __obj* volatile *my_free_list;
     __obj* result;
     __obj* current_obj, next_obj;
@@ -185,11 +189,14 @@ void* __default_alloc_template<threads>::refile(size_t n) {
     // if only alloc a block, then return it to caller
     if (1 == nobjs) 
         return chunk;
+
     my_free_list = free_list + freelist_index(n);
     result = (__obj*) chunk;
 
-    *my_free_list = next_obj = (obj*) (chunk+n);
+    next_obj = (__obj*)(chunk+n);
+    *my_free_list = next_obj;
 
+    // set the tail of allocated freelist is NULL(i.e, 0)
     for (i = 1; ; ++i) {
         current_obj = next_obj;
         next_obj = (__obj*)((char*)next_obj + n);
@@ -201,6 +208,67 @@ void* __default_alloc_template<threads>::refile(size_t n) {
         }
     }
     return result;
+}
+
+// alloc \nobjs __obj with size of \size from memory pool
+// Be Careful: the value of \nobjs maybe changed, it will return the real amount of allocated chunks
+char* __default_alloc_template::__chunk_alloc(size_t size, int &nobjs) {
+
+    char *result;
+    size_t total_bytes = size*nobjs;
+    size_t bytes_left  = end_free - start_free;
+
+    // if (memory pool can satisfy all space need)
+    // else if (memory pool can satisfy part of space need)
+    // else (memory pool cannot satisfy even a block)
+    if ( bytes_left >= total_bytes ) {
+        result = start_free;
+        start_free += total_bytes;
+        return result;
+    } else if ( bytes_left >= size ) {
+        nobjs = bytes_left/size;
+        total_bytes = size*nobjs;
+        result = start_free;
+        start_free += total_bytes;
+        return result;
+    } else {
+        // \bytes_to_get is the memory size we will add into memory pool this time
+        size_t bytes_to_get = 2*total_bytes + round_up(heap_size>>4);
+
+        // make use of remaining memory in memory pool
+        if (bytes_left > 0) {
+            __obj* volatile *my_free_list = freelist+freelist_index(bytes_left);
+            ((__obj*)start_free)->free_list_link = *my_free_list;
+            *my_free_list = (__obj*) start_free;
+        }
+
+        start_free = (char*)malloc(bytes_to_get);
+
+        if (0 == start_free) {
+            // malloc fails
+            int i;
+            __obj *volatile *my_free_list, *p;
+            for (i = size; i <= __MAX_BYTES; i += __ALIGN) {
+                my_free_list = free_list  + freelist_index(i);
+                p = *my_free_list;
+                if ( 0 != p ) {
+                    // if the size bigger than the required size have extra space
+                    // then we will move it into memory pool than call __chunk_alloc again
+                    *my_free_list = p->free_list_link;
+                    start_free = (char*)p;
+                    end_free   = start_free+i;
+                    return (__chunk_alloc(size, nobjs));
+                }
+            }
+            end_free = 0;
+            start_free = (char*)malloc_alloc::allocate(bytes_to_get);
+        }
+
+        heap_size += bytes_to_get;
+        end_free  = start_free + bytes_to_get;
+
+        return __chunk_alloc(bytes, nobjs);
+    }
 }
 
 }
