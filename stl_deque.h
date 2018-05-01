c6648c44ac61d15ce05869ef4f263e0ec5d8c446
Daghlny <Daghlny@gmail.com>
2018-05-01 17:25:28 +0800

    begin writing deque

--- stl_deque.h
+++ stl_deque.h
@@ -10,5 +10,31 @@
 
 namespace ustl{
 
+template<class T, class Allocator>
+class __deque_base {
+    public:
+        __deque_base(size_t __len) {
+            _data = Allocatlr::alloc(sizeof(T*)*__len);
+            _len  = __len;
+        }
+
+    private:
+        T** _data;
+        size_t _len;
+};
+
+template<class T>
+class __deque_iterator {
+    public:
+
+
+};
+
+template<class T, class Allocator>
+class deque{
+    public:
+        typedef __deque_iterator<T> iterator; 
+};
+
 }
 
---
+++ stl_deque.tcc
@@ -0,0 +1,17 @@
+
+#pragma once
+
+#include <cstddef>
+#include <type_traits>
+#include <iostream>
+
+#include "stl_iterator.h"
+#include "stl_allocator.h"
+#include "stl_simple_allocator.h"
+
+namespace ustl {
+
+
+
+}
+
