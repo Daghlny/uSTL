af3efcbdf5f4b73f96c2b2e2c2a7ad92caa9cd1d
Daghlny <Daghlny@gmail.com>
2018-05-26 22:12:18 +0800

    write some func in priority_queue

--- bits/stl_queue.h
+++ bits/stl_queue.h
@@ -46,4 +46,48 @@ class queue {
         Container c;
 };
 
+template<class T, class Container = ustl::vector<T>, class Compare = ustl::less<T> >
+class priority_queue
+{
+    public:
+        typedef Container                   container_type;
+        typedef Container::value_type       value_type;
+        typedef Container::size_type        size_type;
+        typedef Container::reference        reference;
+        typedef Container::const_reference  const_reference;
+
+        typedef priority_queue<T, Container, Compare> _Self;
+
+        priority_queue(const _Self& other): c(other.c), compare(other.cmp){}
+        _Self& operator=(const _Self& other){ 
+            c = other.c; 
+            cmp = other.cmp; 
+            return *this;
+        }
+
+    protected:
+        Container c;
+        Compare   cmp;
+
+    public:
+        const_reference     top() const {return c.front();}
+        bool                empty() const {return c.empty();}
+        size_type           size() const {return c.size();}
+
+        void push(const value_type& value) {
+            c.push_back(value);
+            ustl::push_heap(c.begin(), c.end(), cmp);
+        }
+
+        void pop() {
+            ustl::pop_heap(c.begin(), c.end(), cmp);
+            c.pop_back();
+        }
+
+        void swap(_Self& other) {
+            c.swap(other.c);
+        }
+};
+
 }
+
